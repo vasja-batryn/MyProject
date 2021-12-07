aa96c22d821354d818cf53414dc78c488e184896
vasja-batryn <vasjabat@icloud.com>
2021-12-08 01:14:29 +0200

    ABOUT US

---
+++ js/script.js
@@ -0,0 +1,98 @@
+const upBtn = document.querySelector('.up');
+const downBtn = document.querySelector('.down');
+const container = document.querySelector('.conta');
+const sidebar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide');
+const slideCount = mainSlide.querySelectorAll('div').length;
+
+let activeSlideIndex = 0;
+
+sidebar.style.top = `-${(slideCount - 0.4) * 500}px`;
+
+upBtn.addEventListener('click', () => {
+  changeSlide('up');
+});
+
+downBtn.addEventListener('click', () => {
+  changeSlide('down');
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'ArrowUp') {
+    changeSlide('up');
+  } else if (event.key === 'ArrowDown') {
+    changeSlide('down');
+  }
+});
+
+function changeSlide(direction) {
+  if (direction === 'up') {
+    activeSlideIndex++;
+    if (activeSlideIndex === slideCount) {
+      activeSlideIndex = 0;
+    }
+  } else if (direction === 'down') {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slideCount - 1;
+    }
+  }
+
+  const heightC = container.clientHeight;
+
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * heightC}px)`;
+  sidebar.style.transform = `translateY(${activeSlideIndex * heightC}px)`;
+}
+
+
+let navScrollTop = 0;
+const navbar = document.getElementById("navbar");
+window.addEventListener("scroll", function () {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > navScrollTop) {
+    navbar.style.top = "-82px";
+    navbar.style.transition = "all 0.2s ease-in-out";
+  }
+  else {
+    navbar.style.top = "0";
+    navbar.style.transition = "all 0.5s ease-in-out";
+  }
+  navScrollTop = scrollTop;
+})
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll);
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+				animItem.classList.add('_active');
+			} else {
+				if (!animItem.classList.contains('_anim-no-hide')) {
+					animItem.classList.remove('_active');
+				}
+			}
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	}
+
+	setTimeout(() => {
+		animOnScroll();
+	}, 300);
+}
